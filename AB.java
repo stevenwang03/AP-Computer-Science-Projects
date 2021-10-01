d484c25bd2011a2e85387ace270a19e0631abf9a
Steven Wang <49597640+stevenwang03@users.noreply.github.com>
2021-10-01 01:45:12 -0400

    Create AB.java

---
+++ AB.java
@@ -0,0 +1,33 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Steven Wang
+
+public class AB
+{
+	public static boolean check( String s, String a, String b)
+	{
+		if (a.)
+		return false;
+	}
+}
+
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class AplusRunner
+{
+	public static void main( String args[] )
+	{
+		System.out.println( AB.check( "chicken", "a", "b" ) );
+		System.out.println( AB.check( "frog", "f", "g" ) );
+		System.out.println( AB.check( "chicken", "c", "k" ) );
+		System.out.println( AB.check( "apluscompsci", "a", "s" ) );
+		System.out.println( AB.check( "apluscompsci", "a", "p" ) );
+		System.out.println( AB.check( "apluscompsci", "s", "c" ) );
+		System.out.println( AB.check( "apluscompsci", "c", "p" ) );
+		
+	}
+}
+
