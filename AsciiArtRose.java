a2baa3f61c355fd442bab1bcece3880172133450
Steven Wang <49597640+stevenwang03@users.noreply.github.com>
2021-10-01 01:49:56 -0400

    Create AsciiArtRose.java

---
+++ AsciiArtRose.java
@@ -0,0 +1,23 @@
+public class AsciiArt
+{
+    public static void main(String args[]) 
+ {
+        System.out.println("\t\t\t            _____");
+        System.out.println("\t\t\t	      /  ___  \\");
+        System.out.println("\t\t\t	    /  /  _  \\  \\");
+        System.out.println("\t\t\t	  /( /( /(_)\\ )\\ )\\");
+        System.out.println("\t\t\t	 (  \\  \\ ___ /  /  )");
+        System.out.println("\t\t\t	 (    \\ _____ /    )");
+        System.out.println("\t\t\t	 /(               )\\");
+        System.out.println("\t\t\t	|  \\             /  |");
+        System.out.println("\t\t\t	|    \\ _______ /    |");
+        System.out.println("\t\t\t	 \\    / \\   / \\    /");
+        System.out.println("\t\t\t	   \\/    | |    \\/");
+        System.out.println("\t\t\t	         | |");
+        System.out.println("\t\t\t	         | |");
+        System.out.println("\t\t\t	         |_|");
+
+    }
+}
+
+
