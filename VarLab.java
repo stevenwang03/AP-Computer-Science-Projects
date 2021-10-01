5b021c74074b552416ab630b6f45dfb38d66a72d
Steven Wang <49597640+stevenwang03@users.noreply.github.com>
2021-10-01 01:51:35 -0400

    Create VarLab.java

---
+++ VarLab.java
@@ -0,0 +1,68 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Steven
+//9/12/18
+//AP CS
+//Variables Lab
+
+public class var_lab
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte bite = 110;
+		short shorty = -2000;
+		int internet = 98765;
+		long longitude = 12345678L;
+
+
+		//decimal variables
+		float floaties = 53.876f;
+		double doubleTrouble = 12.9;
+
+
+		//other integer types
+		char charizard = 83;
+
+		//other types
+		boolean booleansss = false;
+		String stringCheese = "MHS CompSci";
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Steven Wang            09/12/18*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - bite = "+bite+"\t\t*");
+		System.out.println("*16 bit - shorty = "+shorty+"\t*");
+		System.out.println("*32 bit - internet = "+internet+"\t*");
+		System.out.println("*64 bit - longitude = "+longitude+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*          real types           *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floaties = "+floaties+"\t*");
+		System.out.println("*64 bit - doubleTrouble = "+doubleTrouble+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charizard = "+charizard+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*         other types           *");
+		System.out.println("*                               *");
+		System.out.println("*booleansss = "+booleansss+"\t\t*");
+		System.out.println("*stringCheese = "+stringCheese+"\t*");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+
+	}
+}
+
+
